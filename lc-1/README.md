2a1b92931cdb9fea2e51f2e4f773042a8be2ce8a
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2024-06-09 00:50:57 +0700

    Update

---
+++ lc-1/README.md
@@ -0,0 +1,5 @@
+# TWO SUM
+[Problem link](https://leetcode.com/problems/two-sum/)
+
+### IDEA
+- Given an element and a target, we can find the diff between them, if there is any element == diff -> it is the other number in two-sum
---
+++ lc-1/fast.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for idx, num in enumerate(nums):
+            if num in hashmap:
+                return [hashmap[num], idx]
+            hashmap[target-num] = idx
